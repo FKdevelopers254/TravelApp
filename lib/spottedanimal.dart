879f6693126745ece7c19034332c3fbe122edf7b
Francis Kitema Nzoka <fkdevelopers254@gmail.com>
2023-11-26 22:12:37 +0300

    Add files via upload

--- lib/spottedanimal.dart
+++ lib/spottedanimal.dart
@@ -1,6 +1,8 @@
  import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:lottie/lottie.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -22,6 +24,7 @@ class AnimalSpottedPage extends StatefulWidget {
  }
 
 class _AnimalSpottedPageState extends State<AnimalSpottedPage> {
+  final mapController = MapController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,54 +48,77 @@ class _AnimalSpottedPageState extends State<AnimalSpottedPage> {
             return Center(child: CircularProgressIndicator());
           }
 
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              final document = snapshot.data!.docs[index];
-              final animalName = document['animalName'];
-              final date = document['date'];
-              final time = document['time'];
-              final latitude = document['latitude'];
-              final longitude = document['longitude'];
-              final imagepath = document['imagepath'];
 
-              return ListTile(
-                leading: Lottie.asset('$imagepath',width: 50,height: 70),
-                title: Text(animalName),
-                subtitle: Text('$date at $time'),
-                trailing: GestureDetector(
-                  onLongPress: ()async {
+          if (snapshot.data is QuerySnapshot) { // Check if the data is of type QuerySnapshot
+            QuerySnapshot querySnapshot = snapshot.data as QuerySnapshot;
+            return ListView.builder(
+              itemCount: querySnapshot.docs.length, // Now you can safely access docs
+              itemBuilder: (context, index) {
+                final document = querySnapshot.docs[index];
+                final animalName = document['animalName'];
+                final date = document['date'];
+                final time = document['time'];
+                final double latitude = document['latitude'];
+                final double longitude = document['longitude'];
+                final imagepath = document['imagepath'];
 
-
-
-                    final url = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
-
-                    if (await canLaunchUrl(Uri.parse(url))) {
-                      final uri = Uri.parse(url);
-                      await launchUrl(uri);
-                    }
-                    else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text('Could not open maps app'),
-                      ));
-                    }
-
-                  },
-                  child: IconButton(
-                    icon: Icon(Icons.map),
-
-                    onPressed: () async {
-                      openMaps(latitude, longitude);
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: Lottie.asset('$imagepath',width: 50,height: 70),
+                      title: Text(animalName),
+                      subtitle: Text('$date at $time'),
+                      trailing: GestureDetector(
+                        onLongPress: ()async {
 
 
-                    },
-                  ),
-                ),
-              );
-            },
-          );
+
+                          final url = 'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+
+                          if (await canLaunchUrl(Uri.parse(url))) {
+                            final uri = Uri.parse(url);
+                            await launchUrl(uri);
+                          }
+                          else {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Could not open maps app'),
+                            ));
+                          }
+
+                        },
+                        child: IconButton(
+                          icon: Icon(Icons.map),
+
+                          onPressed: () async {
+                            openMaps(latitude, longitude);
+
+
+                          },
+                        ),
+                      ),
+                    ),
+
+                  ],
+                );
+
+              },
+            );
+
+
+
+
+          } else {
+            return Center(child: Text('No data available'));
+          }
+
+
         },
       ),
     );
   }
 }
+
+
+
+
+
